3bc6935bda981c1faff7301f9a16261e6cd9eda3
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-08-29 09:44:25 -0600

    commitW5

--- ejercicio3.py
+++ ejercicio3.py
@@ -36,7 +36,7 @@ class proveedor:
         print(f"Nombre del producto: {self.nombreProducto}")
         print(f"Nombre del producto: {self.cantidad}")
         print(f"Precio de compra: {self.precioCompra}")
-        print(f"Precio sugerido: {self.precioSugerido()}")
+        print(f"Precio sugerido: {self.precioSugerido}")
         
 class tienda:
     def __init__(self): 
@@ -53,6 +53,23 @@ class tienda:
         nombreCli = input("Ingrese el nombre del cliente: ")
         self.my_stock[nombre] = {"nombre": nombre, "precio": precio, "cantidad": cantidad, "cliente":nombreCli}
         print(f"producto {nombre} agregado al inventario.") 
+        
+    def ingresarProveedor(self):
+        nombreProveedor = input("Ingrese el nombre del proveedor: ")
+        numContacto = input("Ingrese el numero de contacto del proveedor: ")
+        nombreProducto = input("Ingrese el nombre del producto: ")
+        precioCompra = float(input("Ingrese el precio de compra del producto: "))
+        cantidad = int(input("Ingrese la cantidad del producto: "))
+        self.proveedores.append(proveedor(nombreProveedor, numContacto, nombreProducto, precioCompra, cantidad))
+        
+    def mostrar_proveedores(self):
+        if not self.proveedores:
+            print("No hay proveedores registrados")
+        else:
+            for i in self.proveedores:
+                i.mostrar_datos()
+            
+        
     
     def mostrar_stock(self): # funcion para mostrar los datos
         if not self.my_stock:
@@ -90,3 +107,44 @@ class tienda:
         else:
             print("No hay suficiente efectivo.")
             
+
+    def menu(self): # el menu
+        while True:
+            print("---------------------")
+            print("BIENVENIDO")
+            print("---------------------")
+            print("\nMenu:")
+            print("1. Ingresar productos")
+            print("2. Mostrar stock")
+            print("3. Ingresar proveedor")
+            print("4. Mostrar proveedores")
+            print("5. Comprar")
+            print("6. Salir")
+            print("------------------------")
+            opcion = input("Ingrese una opcion: ") # opcion ayuda al usuario a esocger que hacer
+            match opcion: # matcheo esa opcion, para ejecutar una sentencia dependiendo el caso
+                case "1": 
+                    print("------------------------")
+                    self.ingresar_producto()
+                case "2": 
+                    print("------------------------")
+                    self.mostrar_stock()
+                case "3":
+                    print("------------------------")
+                    self.ingresarProveedor()
+                case "4":
+                    print("------------------------")
+                    self.mostrar_proveedores()
+                case "5": 
+                    print("------------------------")
+                    self.comprar()
+                case "6":
+                    print("------------------------")
+                    print("Gracias por utilizar el sistema de inventario")
+                    break 
+                case _:
+                    print("------------------------")
+                    print("Opcion invalida. Por favor, ingrese una opcion valida.")
+                    continue 
+tienda = tienda() 
+tienda.menu()
