44bbccefa0a1aa92866b2063f22d4e837073b693
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-08-29 09:47:22 -0600

    commitw6

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+"""
+Un biólogo veterinario es el encargado de cuidar a los animales de un zoológico el posee su registro 
+donde va el dato de cada animal por sus características y el área del zoológico donde se encuentran,
+cada cierto tiempo hace sus reportes donde enlista todos los animales del zoológico.
+Al ser veterinario se encarga también de la salud de los animales, 
+por lo cual enlista aquellos que están en tratamiento,
+que dosis y cada cuanto tiempo se debe medicar a un animal.
+"""
