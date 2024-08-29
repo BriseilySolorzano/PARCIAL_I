ec6c329d3571ca6da74fd5b1098a41699ce0b408
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-08-29 09:16:01 -0600

    commitW2

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,31 @@ A su vez, con una calculadora le da el total a cada cliente y les da su respecti
 propón una solución dentro de tu programa para ayudarle.
 """
 
+class producto:
+    def __init__(self, nombre, precio, cantidad): # creo el constructor
+        self.nombre = nombre 
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def mostrar_datos(self): # muestra los datos organizadamente 
+        print(f"Nombre: {self.nombre}")
+        print(f"Precio: {self.precio}")
+        print(f"Cantidad: {self.cantidad}")        
+        
+class proveedor:
+    def __init__(self, nombreProveedor, numContacto, nombreProducto, precioCompra):
+        self.nombreProveedor = nombreProveedor
+        self.numContacto = numContacto
+        self.nombreProducto = nombreProducto
+        self.precioCompra = precioCompra
+        self.precioSugerido = self.precioSugerido()
+        
+    def precioSugerido(self):
+        return (self.precioCompra*0.25)+self.precioCompra
+    
+    def mostrar_datos(self):
+        print(f"Nombre del proveedor: {self.nombreProveedor}")
+        print(f"Numero de contacto: {self.numContacto}")
+        print(f"Nombre del producto: {self.nombreProducto}")
+        print(f"Precio de compra: {self.precioCompra}")
+        print(f"Precio sugerido: {self.precioSugerido()}")
