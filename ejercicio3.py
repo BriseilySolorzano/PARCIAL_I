48c46dab8f22daf78eb4747589950c42913c43a8
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-08-29 09:07:40 -0600

    commitW1

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+"""
+Una tienda local vende diversos productos, cada vez que un cliente hace una compra niña mary se encarga de anotarlo en una libreta. 
+A su vez, con una calculadora le da el total a cada cliente y les da su respectivo vuelto en caso de necesitarlo. 
+ Niña mary también se encarga de atender a los proveedores que le dan cierta cantidad de producto y un precio sugerido de venta, 
+propón una solución dentro de tu programa para ayudarle.
+"""
+
